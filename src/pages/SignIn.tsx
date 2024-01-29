17d6dcb026351291522741d5ff6da665a8619889
esoby <141106@naver.com>
2024-01-29 17:28:00 +0900

    💄 Update the container padding value

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -72,7 +72,7 @@ const SignInPage = () => {
 
   return (
     <div>
-      <form className="flex flex-col items-center gap-5 pt-20">
+      <form className="flex flex-col items-center gap-5 p-20">
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           Sign in
         </h2>
