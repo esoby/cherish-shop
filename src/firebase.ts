d73690a96d4062e50b0211d5178c35b3ee11fc56
esoby <141106@naver.com>
2024-01-31 03:30:11 +0900

    💡 Update comments in firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,10 @@
-// Import the functions you need from the SDKs you need
+// Import the functions from the SDKs
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
