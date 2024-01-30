26b2d31f79da8edea49cd897aab60048697c21c9
esoby <141106@naver.com>
2024-01-31 03:41:21 +0900

    ♻️ Refactor Firebase upload logic into Custom Hook

--- src/pages/ProductUpload.tsx
+++ src/pages/ProductUpload.tsx
@@ -4,15 +4,22 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Toaster } from "@/components/ui/toaster";
 import { useToast } from "@/components/ui/use-toast";
-import { db, storage } from "@/firebase";
-import { redirectIfNotAuthorized } from "@/util/redirectIfNotAuthorized";
 import { Label } from "@radix-ui/react-label";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { ImagePlus } from "lucide-react";
+
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import * as yup from "yup";
+import { redirectIfNotAuthorized } from "@/util/redirectIfNotAuthorized";
+import { useDataUpload } from "@/hooks/useDataUpload";
+import { useImageUpload } from "@/hooks/useImageUpload";
 
 const ProductUpload = () => {
   const user = useAuth();
@@ -20,21 +27,25 @@ const ProductUpload = () => {
 
   const { toast } = useToast();
 
-  const initialVal = {
+  const initialInputVal = {
     productName: "",
     productPrice: 0,
     productQuantity: 0,
     productDescription: "",
-    productCategory: "",
   };
 
   // 사용자 입력 값
-  const [inputValues, setInputValues] = useState(initialVal);
-  const { productName, productPrice, productQuantity, productDescription, productCategory } =
-    inputValues;
+  const [inputValues, setInputValues] = useState(initialInputVal);
+  const { productName, productPrice, productQuantity, productDescription } = inputValues;
+  const [productCategory, setProductCategory] = useState("");
+
+  // input file list
   const [selectedFile, setSelectedFile] = useState<FileList | null>();
 
-  const [imageList, setImageList] = useState<string[]>([]);
+  // upload image and get storage url list
+  const { imageURLs, uploadImages, resetImageURLs } = useImageUpload();
+
+  // input file ref
   const fileInput = useRef<HTMLInputElement>(null);
 
   // 상품 등록 버튼 disabled
@@ -43,31 +54,40 @@ const ProductUpload = () => {
   // 상품 등록 실패 메세지
   const [errorMsg, setErrorMsg] = useState("");
 
+  const { uploadData } = useDataUpload();
+
   // schema for validation
   const requiredSchema = yup.object().shape({
     productName: yup.string().required(),
     productPrice: yup.string().required(),
     productQuantity: yup.string().required(),
     productDescription: yup.string().required(),
-    productCategory: yup.string().required(),
   });
+
+  const categorySchema = yup.string().required();
 
   // schema for number field validation
   const numberSchema = yup.number().integer().min(1);
 
-  useEffect(() => {
-    if (
+  const chkAllValid = () => {
+    return (
       requiredSchema.isValidSync({ ...inputValues }) &&
-      imageList.length > 0 &&
+      imageURLs.length > 0 &&
       numberSchema.isValidSync(inputValues.productPrice) &&
-      numberSchema.isValidSync(inputValues.productQuantity)
-    ) {
+      numberSchema.isValidSync(inputValues.productQuantity) &&
+      categorySchema.isValidSync(productCategory)
+    );
+  };
+
+  // Enable / Disabled button
+  useEffect(() => {
+    if (chkAllValid()) {
       setErrorMsg("");
       setBtnDisabled(false);
     } else {
       setBtnDisabled(true);
     }
-  }, [imageList, inputValues]);
+  }, [imageURLs, inputValues]);
 
   // text input onChange
   const onChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -90,33 +110,23 @@ const ProductUpload = () => {
     setInputValues({ ...inputValues, [inputName]: tmp });
   };
 
+  // image storage upload func
   const uploadImage = (event: ChangeEvent<HTMLInputElement>) => {
     const fileList = event.target.files;
     setSelectedFile(fileList);
   };
 
-  // image storage upload
+  // Upload image to storage as selectedFile change
   useEffect(() => {
     if (selectedFile) {
-      const uploadPromises = Array.from(selectedFile).map((file) => {
-        let fileName = file.name.split(".")[0] + new Date().getTime();
-        const imageRef = ref(storage, `${user?.userId}/${fileName}`);
-        return uploadBytes(imageRef, file).then(() => getDownloadURL(imageRef));
-      });
-      Promise.all(uploadPromises).then((downloadURLs) => setImageList(downloadURLs));
+      uploadImages(selectedFile);
     }
   }, [selectedFile]);
 
+  // product upload
   const uploadProduct = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
-    if (
-      !(
-        requiredSchema.isValidSync({ ...inputValues }) &&
-        imageList.length > 0 &&
-        numberSchema.isValidSync(inputValues.productPrice) &&
-        numberSchema.isValidSync(inputValues.productQuantity)
-      )
-    ) {
+    if (!chkAllValid()) {
       setErrorMsg("입력하지 않은 정보가 존재합니다.");
       return;
     }
@@ -124,23 +134,20 @@ const ProductUpload = () => {
       const newProduct = {
         sellerId: user?.userId,
         ...inputValues,
-        productPrice: productPrice,
-        productQuantity: productQuantity,
-        productImage: imageList,
-        createdAt: serverTimestamp(),
-        updatedAt: serverTimestamp(),
+        productCategory: productCategory,
+        productImage: imageURLs,
       };
-      const collectionRef = collection(db, "products");
-      await addDoc(collectionRef, newProduct);
+      await uploadData("products", newProduct);
       toast({
         description: "상품이 등록되었습니다!",
       });
-      setInputValues(initialVal);
-      setImageList([]);
+
+      // input 초기화
+      setInputValues(initialInputVal);
       setSelectedFile(null);
-      if (fileInput.current) {
-        fileInput.current.value = "";
-      }
+      resetImageURLs();
+      setProductCategory("");
+      if (fileInput.current) fileInput.current.value = "";
     } catch (error: unknown) {
       toast({
         variant: "destructive",
@@ -151,14 +158,15 @@ const ProductUpload = () => {
 
   return (
     <div>
+      <Link to={`/products/${user?.userId}`}>👉🏻 뒤로가기</Link>
       <form className="flex flex-col items-center gap-5 p-20">
         <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           상품 등록
         </h2>
         <div className="relative w-4/5 bg-slate-200 h-80 p-4 mt-5">
           <div className="relative flex gap-4  w-full h-full overflow-scroll">
-            {imageList.length > 0 ? (
-              imageList.map((img, idx) => (
+            {imageURLs.length > 0 ? (
+              imageURLs.map((img, idx) => (
                 <img className="w-72 object-contain" src={img} key={idx}></img>
               ))
             ) : (
@@ -194,6 +202,22 @@ const ProductUpload = () => {
             onChange={onChange}
           />
         </div>
+
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <p>상품 카테고리</p>
+          <Select value={productCategory} onValueChange={(value) => setProductCategory(value)}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="선택" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="Category1">Category1</SelectItem>
+              <SelectItem value="Category2">Category2</SelectItem>
+              <SelectItem value="Category3">Category3</SelectItem>
+              <SelectItem value="Category4">Category4</SelectItem>
+              <SelectItem value="Category5">Category5</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
         <div className="grid w-full max-w-sm items-center gap-1.5">
           <Label htmlFor="productPrice">상품 가격</Label>
           <Input
@@ -223,21 +247,10 @@ const ProductUpload = () => {
             onChange={onChange}
           />
         </div>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="productCategory">상품 카테고리</Label>
-          <Input
-            type="text"
-            id="productCategory"
-            placeholder="product category"
-            value={productCategory}
-            onChange={onChange}
-          />
-        </div>
         <small className="text-sm font-medium text-red-400">{errorMsg}</small>
         <Button className="w-96" onClick={uploadProduct} disabled={btnDisabled}>
           상품 등록
         </Button>
-        <Link to={`/products/${user?.userId}`}>👉🏻 뒤로가기</Link>
         <Toaster />
       </form>
     </div>
