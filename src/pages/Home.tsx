940a21b2043d232680b27d7d1a9f940b93598ef7
esoby <141106@naver.com>
2024-01-31 03:32:12 +0900

    ✨ Update temporary page structure

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,25 @@
 import { useAuth } from "@/AuthContext";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   const user = useAuth();
 
+  useEffect(() => {}, [user]);
   return (
     <div>
-      <h1>Home Page</h1>
-      <div>{user?.nickname}님 안녕하세요</div>
-      <Link to={`/mypage/${user?.userId}`}>마이페이지</Link>
+      <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Home
+      </h2>
+      {user ? (
+        <p>
+          <Link to={`/mypage/${user?.userId}`}>👉🏻 마이페이지</Link>
+        </p>
+      ) : (
+        <p>
+          <Link to={`/signin`}>👉🏻 로그인</Link>
+        </p>
+      )}
     </div>
   );
 };
