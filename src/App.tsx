b4c2ab94e1d1204de2f4dba79e9ed7a0a9321657
esoby <141106@naver.com>
2024-01-31 23:54:14 +0900

    ✨ Add QueryClientProvider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import Router from "./routes/Router";
 import { AuthProvider } from "./AuthContext";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const App = () => (
-  <>
-    <AuthProvider>
-      <Router />
-    </AuthProvider>
-  </>
-);
+const App = () => {
+  const queryClient = new QueryClient();
+  return (
+    <div>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Router />
+        </AuthProvider>
+      </QueryClientProvider>
+    </div>
+  );
+};
 
 export default App;
