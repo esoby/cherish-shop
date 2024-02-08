36d3a92330d0f9bbdc140b25eb1e99697bbe53e5
esoby <141106@naver.com>
2024-02-08 23:37:41 +0900

    💄 Modify cart link style

--- src/components/Common/NavBar.tsx
+++ src/components/Common/NavBar.tsx
@@ -28,8 +28,8 @@ const NavBar = () => {
   );
 
   return (
-    <NavigationMenu>
-      <NavigationMenuList className="w-screen flex border-b pl-10 pr-6 py-4 fixed bg-white ">
+    <NavigationMenu className="left-0 top-0 fixed">
+      <NavigationMenuList className="w-screen flex border-b pl-10 pr-6 py-4 bg-white ">
         <NavigationMenuItem className="flex-grow h-">
           <Link to={"/"}>
             <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">히커머스</h3>
@@ -55,9 +55,9 @@ const NavBar = () => {
               <NavigationMenuItem>
                 <Sheet>
                   <SheetTrigger asChild className={navigationMenuTriggerStyle()}>
-                    <div className="cursor-pointer relative">
+                    <div className="cursor-pointer relative pr-7">
                       <ShoppingCart />
-                      <div className="w-4 h-4 bg-red-500 rounded-full text-[10px] flex justify-center items-center text-white absolute right-1 top-1">
+                      <div className="w-4 h-4 bg-red-500 rounded-full text-[10px] flex justify-center items-center text-white absolute right-3.5 top-1">
                         {cartDatas?.data.length || 0}
                       </div>
                     </div>
