f7c387d5ba98cc939f7b0a24a2e8180df5d9e6bd
esoby <141106@naver.com>
2024-02-13 10:51:14 +0900

    ✨ Implement order cancel feature

--- src/pages/OrderDetail.tsx
+++ src/pages/OrderDetail.tsx
@@ -2,9 +2,17 @@ import { useAuth } from "@/AuthContext";
 import { Button } from "@/components/ui/button";
 import { db } from "@/firebase";
 import { useDataLoad } from "@/hooks/useDataLoad";
-import { Order } from "@/interfaces/Order";
+import { Order, OrderStatus } from "@/interfaces/Order";
 import { Product } from "@/interfaces/Product";
-import { collection, doc, getDoc, query, where } from "firebase/firestore";
+import {
+  collection,
+  doc,
+  getDoc,
+  query,
+  serverTimestamp,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import NavBar from "@/components/Common/NavBar";
@@ -19,6 +27,7 @@ type ProductsInOrder = {
 const OrderDetail = () => {
   const { oid } = useParams();
   const navigate = useNavigate();
+  const [canceled, setCanceled] = useState(false);
   const [orderItems, setOrderItems] = useState<ProductsInOrder[]>();
 
   const { fetchData } = useDataLoad<Order>();
@@ -55,14 +64,40 @@ const OrderDetail = () => {
       const newArr = await Promise.all(promises);
       console.log(newArr);
       setOrderItems(newArr.filter(Boolean) as ProductsInOrder[]);
+
+      return data;
     };
+
     fetch();
   }, []);
 
+  const cancelOrder = async () => {
+    try {
+      if (oid) {
+        const q = query(collection(db, "order"), where("orderGroupId", "==", oid));
+        const { data } = await fetchData(q, null);
+
+        data.map(async (d) => {
+          const newData = {
+            ...d,
+            status: OrderStatus.Cancelled,
+            updatedAt: serverTimestamp(),
+          };
+          const docRef = doc(db, "order", d.id);
+          await updateDoc(docRef, newData);
+        });
+        setCanceled(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
+    <div className="pt-20">
       <NavBar />
-      <h2>주문 완료</h2>
+      <h2>주문 내역</h2>
+      {canceled ? <div>주문 취소됨!</div> : ""}
       {orderItems?.map((item, idx) => (
         <div key={idx}>
           <p>{item.productName}</p>
@@ -71,7 +106,7 @@ const OrderDetail = () => {
           <p>{item.orderQuantity}</p>
         </div>
       ))}
-      <Button onClick={() => navigate("/")}>완료</Button>
+      <Button onClick={() => cancelOrder()}>주문 취소</Button>
     </div>
   );
 };
