4c269118a6aed26d2c84d38746910dde645cb6e1
esoby <141106@naver.com>
2024-02-17 18:49:30 +0900

    🔧 Update compression options to the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,17 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+import compression from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      algorithm: "gzip",
+      threshold: 1024,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src/"),
