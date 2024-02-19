362694de2abc5c2ebeed2b478ad9e08c05109724
esoby <141106@naver.com>
2024-02-20 00:06:35 +0900

    ✨ 🔍 Add HelmetProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import AppRouter from "./routes/Router";
 import { AuthProvider } from "./AuthContext";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { HelmetProvider } from "react-helmet-async";
 
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -8,11 +9,13 @@ const App = () => {
   const queryClient = new QueryClient();
   return (
     <Router>
-      <QueryClientProvider client={queryClient}>
-        <AuthProvider>
-          <AppRouter />
-        </AuthProvider>
-      </QueryClientProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <AuthProvider>
+            <AppRouter />
+          </AuthProvider>
+        </QueryClientProvider>
+      </HelmetProvider>
     </Router>
   );
 };
