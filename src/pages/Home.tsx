7ab44c3878fa1d89fee430b17c41eef7c70b1c9f
esoby <141106@naver.com>
2024-02-20 22:59:31 +0900

    💄 Add main image to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
 import NavBar from "@/components/Common/NavBar";
 import MetaTag from "@/components/Common/SEOMetaTag";
 import { ProductCategory } from "@/components/Product/ProductCategory";
+import mainImg from "@/assets/images/main.webp";
 
 const Home = () => {
-  const category = ["Category1", "Category2", "Category3", "Category4", "Category5"];
-  // const category = ["doll", "figure", "toy", "object", "etc"];
+  const category = ["DOLL", "FIGURE", "TOY", "OBJECT", "ETC"];
 
   return (
     <>
@@ -13,8 +13,16 @@ const Home = () => {
         description="세상 모든 귀여운 것들이 모여있는 공간입니다. 인형부터 피규어까지, 당신의 취향을 저격할 사랑스러운 친구들이 기다리고 있으니 분양받으러 오세요!"
         url="/"
       />
+      <link rel="preload" href={mainImg} as="image" />
       <NavBar />
-      <div className="w-full flex flex-col items-center p-20">
+      <div className="w-full h-96 overflow-hidden relative">
+        <img
+          src={mainImg}
+          alt="main image"
+          className="w-[1080px] max-w-[1080px] absolute -bottom-24"
+        />
+      </div>
+      <div className="w-full">
         {category.map((cat, i) => (
           <ProductCategory category={cat} key={i} />
         ))}
