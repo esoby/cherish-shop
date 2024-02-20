38b4aa5a0f003147ea907f72d0833e017188b3e8
esoby <141106@naver.com>
2024-02-20 22:57:04 +0900

    💄 Add logo image to NavBar

--- src/components/Common/NavBar.tsx
+++ src/components/Common/NavBar.tsx
@@ -17,6 +17,8 @@ import { useDataLoad } from "@/hooks/useDataLoad";
 import { Cart } from "@/interfaces/Cart";
 import { ShoppingCart } from "lucide-react";
 
+import logoImg from "@/assets/images/logo.svg";
+
 const NavBar = () => {
   const { user } = useAuth() || {};
 
@@ -30,11 +32,11 @@ const NavBar = () => {
   );
 
   return (
-    <NavigationMenu className="left-0 top-0 fixed">
-      <NavigationMenuList className="w-screen flex border-b pl-10 pr-6 py-4 bg-white ">
-        <NavigationMenuItem className="flex-grow h-">
+    <NavigationMenu className="left-1/2 -translate-x-1/2 top-0 fixed">
+      <NavigationMenuList className="w-screen flex border-b p-4 pl-6 pr-6 bg-white max-w-[1080px]">
+        <NavigationMenuItem className="flex-grow">
           <Link to={"/"}>
-            <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">히커머스</h3>
+            <img src={logoImg} alt="cherish logo image" className="w-36 -translate-y-2" />
           </Link>
         </NavigationMenuItem>
         {user &&
@@ -44,22 +46,17 @@ const NavBar = () => {
                 href={`/products/${user?.userId}`}
                 className={navigationMenuTriggerStyle()}
               >
-                <p className="text-green-800">PRODUCT</p>
+                <p className="text-pink-700">PRODUCT CENTER</p>
               </NavigationMenuLink>
             </NavigationMenuItem>
           ) : (
             <>
               <NavigationMenuItem>
-                <NavigationMenuLink href={`/community`} className={navigationMenuTriggerStyle()}>
-                  COMMUNITY
-                </NavigationMenuLink>
-              </NavigationMenuItem>
-              <NavigationMenuItem>
                 <Sheet>
                   <SheetTrigger asChild className={navigationMenuTriggerStyle()}>
                     <div className="cursor-pointer relative pr-7">
                       <ShoppingCart />
-                      <div className="w-4 h-4 bg-red-500 rounded-full text-[10px] flex justify-center items-center text-white absolute right-4 top-1">
+                      <div className="w-4 h-4 bg-pink-700 rounded-full text-[10px] flex justify-center items-center text-white absolute right-4 top-1">
                         {cartDatas?.data.length || 0}
                       </div>
                     </div>
