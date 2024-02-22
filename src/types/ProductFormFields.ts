0cca1f4454498735c1750f116258ec7e909c3ecd
esoby <141106@naver.com>
2024-02-22 12:58:27 +0900

    🏷️ Add ProductFormFields types

---
+++ src/types/ProductFormFields.ts
@@ -0,0 +1,8 @@
+export type ProductFormFields = {
+  productName: string;
+  productCategory: string;
+  productPrice: number;
+  productQuantity: number;
+  productDescription: string;
+  productImage: string[];
+};
