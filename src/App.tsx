972655f4d67f1475d60f111ea72d75095c01f6b9
esoby <141106@naver.com>
2024-02-23 09:33:49 +0900

    ✨ Add Alert Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import AppRouter from "./routes/Router";
-import { AuthProvider } from "./AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AlertProvider } from "./context/AlertContext";
+import Alert from "./components/Common/AlertUI";
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -11,9 +13,12 @@ const App = () => {
     <Router>
       <HelmetProvider>
         <QueryClientProvider client={queryClient}>
-          <AuthProvider>
-            <AppRouter />
-          </AuthProvider>
+          <AlertProvider>
+            <AuthProvider>
+              <Alert />
+              <AppRouter />
+            </AuthProvider>
+          </AlertProvider>
         </QueryClientProvider>
       </HelmetProvider>
     </Router>
