9758972ff01afcc2c19bfba2b2be7ebc10c3d7e1
esoby <141106@naver.com>
2024-02-23 09:34:52 +0900

    🚚 Move files

--- src/interfaces/TempInventory.ts
+++
@@ -1,9 +0,0 @@
-import { Timestamp } from "firebase/firestore";
-
-export interface TempInventory {
-  id: string;
-  orderGroupId: string;
-  productId: string;
-  tempQuantity: number;
-  timestamp: Timestamp;
-}
