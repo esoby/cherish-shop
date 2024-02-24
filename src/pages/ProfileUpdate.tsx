e041669491c2bc92f3e49103a21b1757d2616fd5
esoby <141106@naver.com>
2024-02-24 21:46:56 +0900

    ✨ Add profile update page file

---
+++ src/pages/ProfileUpdate.tsx
@@ -0,0 +1,15 @@
+import MainContainer from "@/components/Common/MainContainer";
+import NavBar from "@/components/Common/NavBar";
+
+const ProfileUpdate = () => {
+  return (
+    <>
+      <NavBar />
+      <MainContainer>
+        <h2 className="border-b pb-2 text-2xl font-semibold tracking-tight">프로필 수정</h2>
+      </MainContainer>
+    </>
+  );
+};
+
+export default ProfileUpdate;
